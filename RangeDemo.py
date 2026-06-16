f189e58dac2398fe7900f8e3b23d7ef875689c02
Atharv-T2006 <atharvtimane1@gmail.com>
2026-06-16 09:04:03 +0530

    Add files via upload

---
+++ RangeDemo.py
@@ -0,0 +1,14 @@
+Data=range(5)
+print(Data)
+print(type(Data))
+
+Data1 = range(1,7)  ## start,end
+print(Data1)
+
+Data2 = range(3,9,2)   ## start,end,jump
+print(Data2)
+
+print(len(Data))
+print(len(Data1))
+print(len(Data2))
+
