516e49bfd5092e1f1f6ec7f443c4d42d13a100c8
Atharv-T2006 <atharvtimane1@gmail.com>
2026-06-17 23:13:48 +0530

    Add files via upload

---
+++ IfElifDemo.py
@@ -0,0 +1,15 @@
+print("-------------------------------------------")
+print("----------Ticket pricing software----------")
+print("-------------------------------------------")
+
+print("Please enter your age : ")
+age = int(input())
+
+if(age <= 5):
+    print("Free entry")
+elif(age>5 and age <= 18):
+    print("Ticket price : 900")
+elif(age > 18 and age <=40):
+    print("Ticket price : 1200")
+else:
+    print("Ticket price : 500")
---
+++ IfElseDemo.py
@@ -0,0 +1,7 @@
+print("Enter your Age : ")
+age = int(input())
+
+if(age < 18):
+    print("Sorry you are not allowed")
+else:
+    print("Welcome to the movie")
