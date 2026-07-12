aaa706f3cdd763d1e9ab16e23ea1d06db3b38649
Atharv-T2006 <atharvtimane1@gmail.com>
2026-07-12 15:11:16 +0530

    Add files via upload

---
+++ InstanceVariable1.py
@@ -0,0 +1,22 @@
+class Marvellous:
+    # class variable
+    no1=11
+    no2=12
+    
+    def __init__(self):
+        # instance variable
+        self.value1=21
+        self.value2=51
+
+print(Marvellous.no1)
+print(Marvellous.no2)
+
+# objects / instance creation
+mobj1=Marvellous()
+mobj2=Marvellous()
+mobj3=Marvellous()
+
+print(mobj1.value1)
+print(mobj2.value1)
+
+
