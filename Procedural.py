a812cd5dde1a345f3cb3e8ba37ab7c2100561b9b
Atharv-T2006 <atharvtimane1@gmail.com>
2026-07-13 08:17:54 +0530

    Add files via upload

---
+++ Procedural.py
@@ -0,0 +1,19 @@
+def Addition(no1,no2):
+    ans = no1+no2
+
+    return ans
+
+def Substraction(no1,no2):
+    ans = no1-no2
+
+    return ans
+
+no1=int(input("Enter first number :" ))
+no2=int(input("Enter second number : " ))
+
+ret=Addition(no1,no2)
+print("Addition is : ",ret)
+
+ret=Substraction(no1,no2)
+print("Addition is : ",ret)
+
