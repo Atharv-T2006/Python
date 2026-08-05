b21336edac6ecb46d8df8eea3a0d72d386ab56e0
Atharv-T2006 <atharvtimane1@gmail.com>
2026-08-05 20:41:53 +0530

    Add files via upload

---
+++ Ball_Classification2.py
@@ -0,0 +1,22 @@
+# Rough = 1
+# smooth = 0
+
+#tennis =1
+#cricket=2
+
+
+def main():
+    print('Ball Classification Case Study')
+   # Encoding
+    features=[[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,"1"],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[95,0]]
+
+    Labels=[1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+
+    print("Features are : ",features)
+    print("Lables are : ",Labels)
+
+
+
+
+if __name__=="__main__":
+    main()
